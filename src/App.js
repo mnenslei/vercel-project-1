faf07bca7229421d05ff2963cdd77762eaee99d8
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-12-16 13:07:23 -0500

    personalized

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <main>
       <h1>Coco's App</h1>
-      <button>Coco?</button>
+      <p>I am coco the dog and I love snacks!</p>
+      <button>GIVE ME A TREAT!!!</button>
     </main>
   );
 }
